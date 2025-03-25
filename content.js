86201b70bb333ca2be213a40c44e615b41c8ea39
BabyXeno <djm7992@psu.edu>
2025-03-25 03:27:29 -0400

    Initial commit of game files

---
+++ content.js
@@ -0,0 +1,2 @@
+// content.js (Injects the game into the page)
+document.body.innerHTML = '<iframe src="index.html" style="position:fixed;top:0;left:0;width:100%;height:100%;border:none;z-index:9999;"></iframe>';
